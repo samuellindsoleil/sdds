da4c0050c01ec9a366329668900da7cfe68e540d
Adarsh Dubey <adarsh.dubey@lntinfotech.com>
2021-09-09 12:22:00 +0530

    committing code for Gulp file modification with del task before build.

--- styles/gulpfile.js
+++ styles/gulpfile.js
@@ -1,35 +1,19 @@
 var gulp = require('gulp');
 var run = require('gulp-run');
+const del = require("del");
 
-// import gulp from 'gulp'
-// import run from 'gulp-run-command' 
 
-// const outputFolder = 'dist';
-// const build = series(clean);
-// const start = series(build, serve, watchtask);
-
-// function clean() {
-//     return del(outputFolder);
-//   }
-
-gulp.task('clean', run('rm -rf build'))
-gulp.task('build', ['clean'], run('npm run scss', {
-    env: { NODE_ENV: 'production' }
-  }))
+gulp.task("del", function(){
+    return del(["css"]);
+});
 
 gulp.task('watchtask', function () {
     console.log("command will execute");
-    // watch for scss file (*.scss) changes, in current directory (./)
-    gulp.watch('./scss/light/theme/*.scss', function () {
+    gulp.watch('./scss/light/theme/*.scss', gulp.series("del","css"));
+});
 
-        console.log("command will execute here after changes saved in Scss file");
-
-        // run an npm command called `test`, when above scss file changes
-        // return run('npm run scss').exec();
-        run('npm run scss').exec();
-
-    });
-
-
+gulp.task("css",() => {
+    console.log("Compling sass > css");
+    run('npm run scss').exec();
 });
 
