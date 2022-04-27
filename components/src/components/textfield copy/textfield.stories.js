85532a89cb87429cf58c3f881cbfb48e630a21ab
Albin Larsson <albin@farstun.com>
2022-04-27 20:31:12 +0200

    fix: example of creating clone fields for sdds form element components

---
+++ components/src/components/textfield copy/textfield.stories.js
@@ -0,0 +1,241 @@
+import readme from './readme.md';
+
+export default {
+  title: 'Component/TextField SERIALIZE',
+  parameters: {
+    notes: readme,
+  },
+  argTypes: {
+    placeholderText: {
+      name: 'Placeholder ',
+      description: 'Placeholder text',
+      control: {
+        type: 'text',
+      },
+    },
+    type: {
+      name: 'Type',
+      description: 'Which type of textfield',
+      control: {
+        type: 'radio',
+        options: ['password', 'text'],
+      },
+    },
+    size: {
+      name: 'Size',
+      description: 'Switch between different sizes',
+      control: {
+        type: 'radio',
+        options: ['Default', 'Medium'],
+      },
+    },
+    minWidth: {
+      name: 'Min width',
+      description: 'Toggle min width',
+      control: {
+        type: 'radio',
+        options: ['Default', 'No min width'],
+      },
+    },
+    disabled: {
+      description: 'Set textfield to disabled state',
+      name: 'Disabled',
+      control: {
+        type: 'boolean',
+      },
+    },
+    label: {
+      description: 'Label text for specific textfield',
+      name: 'Label text',
+      control: {
+        type: 'text',
+      },
+    },
+    labelplacement: {
+      description: 'Label can be placed inside the textfield',
+      name: 'Label inside',
+      control: {
+        type: 'boolean',
+      },
+    },
+    helper: {
+      name: 'Helper text',
+      description: 'Add helper text for the textfield',
+      control: {
+        type: 'text',
+      },
+    },
+    textcounter: {
+      name: 'Text counter',
+      description: 'Set a maximum value how long the text can be',
+      control: {
+        type: 'number',
+      },
+    },
+    state: {
+      name: 'State',
+      description: 'Switch between success or error state',
+      control: {
+        type: 'radio',
+        options: ['none', 'success', 'error'],
+      },
+    },
+  },
+  args: {
+    placeholderText: 'Placeholder',
+    type: 'text',
+    size: 'Default',
+    minWidth: 'Default',
+    disabled: false,
+    state: 'default',
+    label: '',
+    labelplacement: false,
+    helper: '',
+  },
+};
+
+const textfieldTemplate = ({
+  type,
+  placeholderText,
+  size,
+  minWidth,
+  disabled,
+  label,
+  labelplacement,
+  state,
+  helper,
+  prefix,
+  suffix,
+  icon,
+  textcounter,
+}) => {
+  let sizeValue;
+  switch (size) {
+    case 'Medium':
+      sizeValue = 'md';
+      break;
+    case 'Default':
+      sizeValue = 'default';
+      break;
+    default:
+      break;
+  }
+  let minWidthValue = false;
+  switch (minWidth) {
+    case 'No min width':
+      minWidthValue = true;
+      break;
+    default:
+      break;
+  }
+
+  return `
+  <div style="width: 208px">
+    <sdds-textfield-serialize
+      type="${type}"
+      size="${sizeValue}"
+      state="${state}"
+      maxlength="${textcounter}"
+      ${label && labelplacement ? `label-inside="${label}"` : ''}
+      ${disabled ? 'disabled' : ''}
+      ${minWidthValue ? 'noMinWidth' : ''}
+      placeholder="${placeholderText}" >
+        ${prefix}
+        ${
+          label && !labelplacement
+            ? `<label slot='sdds-label'>${label}</label>`
+            : ''
+        }
+        ${helper ? `<span slot='sdds-helper'>${helper}</span>` : ''}
+        ${suffix}
+        ${icon}
+    </sdds-textfield-serialize>
+  </div>
+  `;
+};
+
+export const Basic = textfieldTemplate.bind({});
+
+Basic.args = {};
+
+export const Disabled = textfieldTemplate.bind({});
+
+Disabled.args = {
+  disabled: true,
+  placeholderText: 'Disabled',
+};
+
+export const labels = textfieldTemplate.bind({});
+
+labels.args = {
+  label: 'Label text',
+};
+
+export const helper = textfieldTemplate.bind({});
+
+helper.args = {
+  helper: 'Helper text',
+  label: 'Label text',
+};
+
+export const state = textfieldTemplate.bind({});
+
+state.args = {
+  state: 'error',
+  helper: 'Helper text',
+  label: 'Label text',
+};
+
+export const prefix = textfieldTemplate.bind({});
+
+prefix.argTypes = {
+  prefix: {
+    name: 'Prefix',
+    description: 'Add prefix symbol/text before the textfield',
+    control: {
+      type: 'text',
+    },
+  },
+};
+
+prefix.args = {
+  helper: '',
+  label: 'Label text',
+  prefix: '<span slot="sdds-prefix">$</span>',
+};
+
+export const suffix = textfieldTemplate.bind({});
+
+suffix.argTypes = {
+  suffix: {
+    name: 'Suffix',
+    description: 'Add suffix symbol/text after the textfield',
+    control: {
+      type: 'text',
+    },
+  },
+};
+
+suffix.args = {
+  helper: '',
+  label: 'Label text',
+  suffix: '<span slot="sdds-suffix">$</span>',
+};
+
+export const icon = textfieldTemplate.bind({});
+
+icon.argTypes = {
+  icon: {
+    name: 'Icon',
+    description: 'Add icon before or after the textfield',
+    control: {
+      type: 'text',
+    },
+  },
+};
+
+icon.args = {
+  helper: '',
+  label: 'Label text',
+  icon: '<sdds-icon name="scania-cross" slot="sdds-prefix"></sdds-icon>',
+};
---
+++ components/src/components/textfield copy/textfield.tsx
@@ -0,0 +1,185 @@
+import {
+  Component,
+  h,
+  State,
+  Prop,
+  Listen,
+  Event,
+  EventEmitter,
+  Element,
+} from '@stencil/core';
+
+@Component({
+  tag: 'sdds-textfield-serialize',
+  styleUrl: 'textfield.scss',
+  shadow: true,
+})
+export class Textfield {
+  /** Textinput for focus state */
+  textInput?: HTMLInputElement;
+
+  @Element() host: HTMLElement;
+  reflectedInput: HTMLInputElement;
+
+  /** Which input type, text, password or similar */
+  @Prop({ reflect: true }) type: string = 'text';
+
+  /** Label that will be put inside the input */
+  @Prop() labelInside: string = '';
+
+  /** Placeholder text */
+  @Prop() placeholder: string = '';
+
+  /** Value of the input text */
+  @Prop({ reflect: true }) value = '';
+
+  /** Set input in disabled state */
+  @Prop() disabled: boolean = false;
+
+  /** Size of the input */
+  @Prop() size = '';
+
+  /** With setting */
+  @Prop() nominwidth: boolean = false;
+
+  /** Name property */
+  @Prop() name = '';
+
+  @Prop() id = '';
+
+  /** Error state of input */
+  @Prop() state: string;
+
+  /** Max length of input */
+  @Prop() maxlength: number;
+
+  /** Autofocus for input */
+  @Prop() autofocus: boolean = false;
+
+  /** Listen to the focus state of the input */
+  @State() focusInput;
+
+  /** Change event for the textfield */
+  @Event({
+    composed: true,
+    bubbles: true,
+    cancelable: true,
+  })
+  customChange: EventEmitter;
+
+  // Listener if input enters focus state
+  @Listen('focus')
+  handleFocusIn() {
+    this.focusInput = true;
+  }
+
+  // Listener if input leaves focus state
+  @Listen('focusout')
+  handleFocusOut() {
+    this.focusInput = false;
+  }
+
+  // Data input event in value prop
+  handleInput(e): void {
+    this.value = e.target.value;
+    this.reflectedInput.value = this.value;
+  }
+
+  // Change event isn't a composed:true by default in for input
+  handleChange(e): void {
+    this.customChange.emit(e);
+  }
+
+  /** Set the input as focus when clicking the whole textfield with suffix/prefix */
+  handleFocusClick(): void {
+    this.textInput.focus();
+  }
+
+  componentWillLoad() {
+    this.reflectedInput = document.createElement('input');
+    this.reflectedInput.style.display = 'none';
+
+    this.reflectedInput.setAttribute('name', this.name);
+    this.reflectedInput.setAttribute('id', this.id);
+
+    this.host.parentElement.appendChild(this.reflectedInput);
+  }
+
+  render() {
+    return (
+      <div
+        class={`
+        ${this.nominwidth ? 'sdds-form-textfield-nomin' : ''}
+        ${
+          this.focusInput
+            ? 'sdds-form-textfield sdds-textfield-focus'
+            : ' sdds-form-textfield'
+        }
+        ${this.value.length > 0 ? 'sdds-textfield-data' : ''}
+        ${
+          this.labelInside.length > 0
+            ? 'sdds-textfield-container-label-inside'
+            : ''
+        }
+        ${this.disabled ? 'sdds-form-textfield-disabled' : ''}
+        ${this.size == 'md' ? 'sdds-form-textfield-md' : ''}
+        ${
+          this.state == 'error' || this.state == 'success'
+            ? `sdds-form-textfield-${this.state}`
+            : ''
+        }
+        `}
+      >
+        <slot name="sdds-label" />
+
+        <div
+          onClick={() => this.handleFocusClick()}
+          class="sdds-textfield-container"
+        >
+          <slot name="sdds-prefix" />
+
+          <div class="sdds-textfield-input-container">
+            <input
+              ref={(inputEl) => (this.textInput = inputEl as HTMLInputElement)}
+              class={`${
+                this.size !== 'md'
+                  ? 'sdds-textfield-input'
+                  : 'sdds-textfield-input-md'
+              }`}
+              type={this.type}
+              disabled={this.disabled}
+              placeholder={this.placeholder}
+              value={this.value}
+              autofocus={this.autofocus}
+              maxlength={this.maxlength}
+              name={this.name}
+              onInput={(e) => this.handleInput(e)}
+              onChange={(e) => this.handleChange(e)}
+            />
+
+            {this.labelInside.length > 0 && (
+              <label class="sdds-textfield-label-inside">
+                {this.labelInside}
+              </label>
+            )}
+          </div>
+          <div class="sdds-textfield-bar"></div>
+
+          <slot name="sdds-suffix" />
+        </div>
+
+        <div class="sdds-textfield-helper">
+          <slot name="sdds-helper" />
+
+          {this.maxlength > 0 && (
+            <div class="sdds-textfield-textcounter">
+              {this.value.length}
+              <span class="sdds-textfield-textcounter-divider"> / </span>
+              {this.maxlength}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
